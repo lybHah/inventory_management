466827ec2cf32dd5690f6e8c450c5521dff5b22a
lybHah <lybHah@git.com>
2016-04-10 18:26:10 +0800

    set mysql database in settings

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventory_management_site',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
